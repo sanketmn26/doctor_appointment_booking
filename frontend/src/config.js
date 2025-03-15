8c7497fa0b28da8010ba513ee36fae562bdad4e8
sanketmn26 <sanketmn26@gmail.com>
2025-03-15 10:12:03 +0530

    updated config url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api/v1";
+// export const BASE_URL = "http://localhost:5000/api/v1";
+export const BASE_URL = "https://doctor-appointment-booking-server.onrender.com/api/v1";
 export const token = localStorage.getItem("token")
